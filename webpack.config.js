113643d4b2d0ac1765e785d27a2b7e7f0ac6ebce
jimous <jimouspeng@xiaoe-tech.com>
2020-08-02 23:16:18 +0800

    1

--- webpack.config.js
+++ webpack.config.js
@@ -16,14 +16,14 @@ module.exports = {
     // module.rules 代表了模块的处理规则
     module: {
         rules: [
-            {
-                test: /\.js$/,
-                enforce: 'pre',
-                use: 'eslint-loader',
-            },
+            // {
+            //     test: /\.js$/,
+            //     enforce: 'pre',
+            //     use: 'eslint-loader',
+            // },
             {
                 // test: /\.css$/, // 正则或者正则数组
-                use: ['style-loader','css-loader'], // webpack打包是按照数组从后往前的顺序将资源交给loader处理的，因此要把最后生效的放在前面。
+                use: ['style-loader','css-loader'], // webpack打包是按照数组从后往前的顺序将资源交给loader处理的，因此要把最后生效的放在前面。 字符串、数组，对象均可
                 // exclude: /node_modules/, //node_modules中的模块不执行该条规则。该配置项通常是必加的，否则可能拖慢整个的打包速度。
                 // include: /src/, // 只对匹配到的模块生效。exclude和include同时存在，exclude的优先级更高。
                 resource: {
@@ -37,6 +37,23 @@ module.exports = {
                     test: /\.js$/,
                     include: /src/,
                     exclude: /node_modules/,
+                }
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_module/,
+                use: {
+                    loader: 'babel-loader', // babel通常属于对所有JS后缀文件设置的规则，所有需要
+                    options: {
+                        cachDirectory: true,
+                        presets: [
+                            [
+                                'env', {
+                                    modules: true,
+                                }
+                            ]
+                        ]
+                    }
                 }
             }
         ]
