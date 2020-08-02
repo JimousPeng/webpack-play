0589dd1941e98cbcbb026415a8ed22f66bbc3d33
jimous <jimouspeng@xiaoe-tech.com>
2020-08-02 23:00:04 +0800

    loader预处理

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,5 +1,5 @@
 import addContent from './add-content';
-document.write('jimous,hhh.<br />');
+document.write('jimous,hhhss.<br />');
 addContent();
 
 // 在导入一个模块时，对CommonJS来说获取的是一份导出值的拷贝，而在ES6 Module中则是值的动态映射，并且这个映射是只读的。
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,49 @@
+const path = require('path');
 module.exports = {
-    entry: './src/index.js',
+    // context可以让entry的编写更加简洁，尤其是多入口的情况下。context可以省略，默认值为当前工程的根目录。
+    context: path.join(__dirname, './src/libs'), // 只能是 String 类型
+    // entry: './index.js', // String Array Object Func 都可以
+    entry: {
+        index: './index.js',
+        second: './test.js',
+        app: './app.js'
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].js',
+        path: path.join(__dirname, 'dist')
+        // publicPath: '/dist/'
+    },
+    // module.rules 代表了模块的处理规则
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                enforce: 'pre',
+                use: 'eslint-loader',
+            },
+            {
+                // test: /\.css$/, // 正则或者正则数组
+                use: ['style-loader','css-loader'], // webpack打包是按照数组从后往前的顺序将资源交给loader处理的，因此要把最后生效的放在前面。
+                // exclude: /node_modules/, //node_modules中的模块不执行该条规则。该配置项通常是必加的，否则可能拖慢整个的打包速度。
+                // include: /src/, // 只对匹配到的模块生效。exclude和include同时存在，exclude的优先级更高。
+                resource: {
+                    // resource -- 加载模块
+                    test: /\.css$/, // 正则或者正则数组
+                    exclude: /node_modules/,
+                },
+                issuer: {
+                    // issuer -- 加载者
+                    // 只让 /src 目录下的js可以引用css
+                    test: /\.js$/,
+                    include: /src/,
+                    exclude: /node_modules/,
+                }
+            }
+        ]
     },
     mode: 'development',
     devServer: {
-        publicPath: '/dist',
+        publicPath: '/assets/',
+        port: 3000
     }
 }
